615f12cb7766af6e1e51319a2037192496cee791
Tarun2474 <tarunrawat7906@gmail.com>
2026-07-22 15:57:45 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -163,7 +163,9 @@ export default function Home() {
             <p className="text-[var(--text-muted)] font-bold">Try adjusting your search query or category filters.</p>
           </div>
         )}
-      </div>
-    </div>
+        <footer className="text-center py-6 text-gray-500 text-sm mt-10">
+  © {new Date().getFullYear()} RAWAT SHOP. All rights reserved.
+</footer>
+    </div> 
   );
 }
