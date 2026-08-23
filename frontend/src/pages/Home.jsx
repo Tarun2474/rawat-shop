a27a85cd81e0ec885c17ae6ad1089de690d52398
Tarun2474 <tarunrawat7906@gmail.com>
2026-08-23 16:04:24 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -172,7 +172,7 @@ export default function Home() {
           </div>
         ) : currentWallpapers.length > 0 ? (
           <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-3 gap-3 md:gap-8">
               {currentWallpapers.map((wp) => (
                 <WallpaperCard 
                   key={wp._id} 
