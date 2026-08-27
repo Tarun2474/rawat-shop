61b633602cfbffaae3f331307e4d2e43a69de880
Tarun2474 <tarunrawat7906@gmail.com>
2026-08-27 10:57:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,8 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 // Test Route
 app.get("/", (req, res) => {
